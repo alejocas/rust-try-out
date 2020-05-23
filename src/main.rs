780d09f9ee6888c6aaab80a2a198e806c47726f8
Alejo <alejocas@experimentality.co>
2020-05-23 06:34:20 -0500

    Graphics configuration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use tcod::colors::*;
+use tcod::console::*;
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+const LIMIT_FPS: i32 = 20;
+
+struct Tcod {
+    root: Root
+}
+
 fn main() {
-    println!("Hello, world!");
+    let root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust - Roguelike game for [UdeA] OS course")
+        .init();
+
+    let mut tcod = Tcod { root };
 }
