c35025984314f186649b3683f5e9809e3b9b2683
Alejo <alejocas@experimentality.co>
2020-05-23 07:11:37 -0500

    Drawing screen and init player configuration

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use tcod::colors::*;
 use tcod::console::*;
 
+// Actual size of the window
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 
-const LIMIT_FPS: i32 = 20;
+const LIMIT_FPS: i32 = 20; // 20 frames per second as maximum
 
 struct Tcod {
     root: Root
 }
 
 fn main() {
-    let root = Root::initializer()
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let root: Root = Root::initializer()
         .font("arial10x10.png", FontLayout::Tcod)
         .font_type(FontType::Greyscale)
         .size(SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -19,4 +22,15 @@ fn main() {
         .init();
 
     let mut tcod = Tcod { root };
+
+    let mut playerX = SCREEN_WIDTH / 2;
+    let mut playerY = SCREEN_HEIGHT / 2;
+
+    while !tcod.root.window_closed() {
+        tcod.root.set_default_foreground(WHITE);
+        tcod.root.clear();
+        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        tcod.root.flush();
+        tcod.root.wait_for_keypress(true);
+    }
 }
